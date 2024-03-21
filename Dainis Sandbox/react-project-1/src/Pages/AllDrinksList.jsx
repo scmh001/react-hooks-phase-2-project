97c9564c0b1c61f42252cde6a551ffee6e4280f0
Shukri Hussein <husseinshukri@outlook.com>
2024-03-21 18:00:53 -0400

    folder fixing

--- Dainis Sandbox/react-project-1/src/Pages/AllDrinksList.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AllDrinksList() {
-  return (
-    <div>AllDrinksList</div>
-  )
-}
--- Dainis Sandbox/react-project-1/src/Pages/IndividualDrinks.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function IndividualDrinks() {
-  return (
-    <div>IndividualDrinks</div>
-  )
-}
