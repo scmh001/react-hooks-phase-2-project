27f61ca8051adf2c5389bc54d4d2abc7774c88eb
Shukri Hussein <husseinshukri@outlook.com>
2024-03-21 09:06:06 -0400

    volume intake tracker implemented

---
+++ react-project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DrinkVolumeInputTracker from './components/DrinkVolumeInputTracker'
+
+function App() {
+  return (
+    <div>
+      <DrinkVolumeInputTracker />
+    </div>
+  )
+}
+
+
+export default App
