37a009dab4024c8d38d0676f8ee733bb9ed0db0f
Shukri Hussein <husseinshukri@outlook.com>
2024-03-21 15:24:04 -0400

    cards implemented

--- react-project-1/src/components/Header.jsx
+++ react-project-1/src/components/Header.jsx
@@ -3,7 +3,7 @@
 const Header = () => {
   return (
     <header className="header">
-      <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" alt="Placeholder" className="logo" />
+      <img src="https://aldianews.com/sites/default/files/2022-10/paradiso-bar_b966fcba_1280x853.jpg" alt="Placeholder" className="logo" />
       <nav>
         <ul className="nav-links">
            
