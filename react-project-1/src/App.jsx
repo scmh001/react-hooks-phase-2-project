c831b09674fcabc3891322bd3c72e6a2cd7fdbfc
Shukri Hussein <husseinshukri@outlook.com>
2024-03-21 09:58:11 -0400

    fresh

--- react-project-1/src/App.jsx
+++ react-project-1/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import DrinkVolumeInputTracker from './components/DrinkVolumeInputTracker'
 
 function App() {
--- react-project-1/src/components/DrinkVolumeInputTracker.jsx
+++ react-project-1/src/components/DrinkVolumeInputTracker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const DrinkVolumeInputTracker = () => {
   const [volumes, setVolumes] = useState([{ ounces: '' }]);
