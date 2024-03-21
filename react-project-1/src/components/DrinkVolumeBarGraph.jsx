31d110facd89bb14c82f2dd9dc76f5de7a573ee9
Shukri Hussein <husseinshukri@outlook.com>
2024-03-21 14:49:53 -0400

    working bar graph react-charts

---
+++ react-project-1/src/components/DrinkVolumeBarGraph.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const DrinkVolumeBarGraph = ({ volumes }) => {
+  const labels = volumes.map((_, index) => `Drink ${index + 1}`);
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Volume (ounces)',
+        data: volumes.map(volume => Number(volume.ounces)),
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+};
+
+export default DrinkVolumeBarGraph;
