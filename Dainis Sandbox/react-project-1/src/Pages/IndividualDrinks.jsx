932a6e0b6bebf8322693161f249260e421e7e285
Shukri Hussein <husseinshukri@outlook.com>
2024-03-21 14:21:32 -0400

    Folders

---
+++ Dainis Sandbox/react-project-1/src/Pages/IndividualDrinks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function IndividualDrinks() {
+  return (
+    <div>IndividualDrinks</div>
+  )
+}
---
+++ react-project-1/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" alt="Placeholder" className="logo" />
+      <nav>
+        <ul className="nav-links">
+           
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
