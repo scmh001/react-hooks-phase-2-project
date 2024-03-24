468274dcc913793ece0d7024d91911922cb4c364
Shukri Hussein <husseinshukri@outlook.com>
2024-03-24 14:05:56 -0400

    update tailwind

--- react-project-1/src/components/DrinkCard.jsx
+++ react-project-1/src/components/DrinkCard.jsx
@@ -71,31 +71,29 @@ useEffect(()=> {
 const ingredientsArray = ingredients.split(', '); 
   return (
     <div className="card bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
-          <NavLink to={`/drink/${id}`} className="block hover:text-blue-500">
-              <h2 className="text-2xl font-bold text-gray-800 p-4">{name}</h2>
-          </NavLink>
-          <button 
-            className='favorite-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110' 
-            onClick={handleFavorite}>
-              {favorited === 'true' ? 'Unfavorite' : 'Favorite'}
-          </button>
+      <NavLink to={`/drink/${id}`} className="block hover:text-blue-500">
+        <h2 className="text-2xl font-bold text-gray-800 p-4">{name}</h2>
+      </NavLink>
+      <button 
+        className='favorite-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110' 
+        onClick={handleFavorite}>
+          {favorited === 'true' ? 'Unfavorite' : 'Favorite'}
+      </button>
       <img src={image} alt={name} className="drink-image w-full h-64 object-cover" />
-          <button className="like-btn" onClick={handleCheers}>{cheers} Cheers!</button>
-              {/* Button to toggle ingredients visibility */}
-          <button className="ingredients-btn bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full inline-flex items-center justify-center mb-4" onClick={toggleIngredients}>
-               Show Ingredients
-          </button>
-            <p className='category text-sm text-gray-600'>{category}</p>
-              {showIngredients ? 
-              (<ul className="list-disc pl-8 mt-4">
-               {ingredientsArray.map((ingredient, index) => (
-              <li key={index} className="text-gray-700">{ingredient}</li>
-                     ))}
-              </ul>)
-              : (
-                  null
-               )}
-      </div>
+      <button className="like-btn" onClick={handleCheers}>{cheers} Cheers!</button>
+      <button className="ingredients-btn bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full inline-flex items-center justify-center mb-4" onClick={toggleIngredients}>
+        Show Ingredients
+      </button>
+      <p className='category text-sm text-gray-600'>{category}</p>
+      {showIngredients && (
+        <ul className="list-disc pl-8 mt-4">
+          {ingredientsArray.map((ingredient, index) => (
+            <li key={index} className="text-gray-700">{ingredient}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+
   );
 }
 
