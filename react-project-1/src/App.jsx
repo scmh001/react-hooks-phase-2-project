8bf65e98015bd682554fc6c145571be73de59b3d
Jasen <jasen.miyamoto@gmail.com>
2024-03-25 09:33:29 -0400

    db edits and header delete

--- react-project-1/src/App.jsx
+++ react-project-1/src/App.jsx
@@ -1,11 +1,10 @@
 import NavigationBar from './components/NavigationBar'
 import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
+
 
 function App() {
   return (
     <div>
-       {/* <Header /> */}
        <NavigationBar />
        <Outlet />
      
