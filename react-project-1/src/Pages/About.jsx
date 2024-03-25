0f7e10cc7162600b3368db0a593887749df2a5a4
Jasen <jasen.miyamoto@gmail.com>
2024-03-25 13:18:14 -0400

    Happy Hour

--- react-project-1/src/Pages/About.jsx
+++ react-project-1/src/Pages/About.jsx
@@ -12,6 +12,10 @@ export default function About() {
         Additionally, we feature a Water Volume Intake Tracker for hydration monitoring and a "Show Ingredients" button for a deeper dive into what makes up each beverage. 
         With the ability to filter drinks by popularity, Drinks Galore serves as a dynamic and interactive glossary for discovering new favorites and engaging with a community of like-minded drink lovers.
       </p>
+      <br></br>
+      <div>
+        <img src='https://c.pxhere.com/photos/3a/88/neon_neon_font_advertisement_advertising_night_light_happy_hour_green-1348764.jpg!d'></img>
+      </div>
     </div>
   )
 }
--- react-project-1/src/Pages/AllDrinksList.jsx
+++ react-project-1/src/Pages/AllDrinksList.jsx
@@ -14,13 +14,8 @@ export default function AllDrinksList() {
     <>
       <h2></h2>
       <Search search={search} handleSearch={handleSearch} />
-<<<<<<< HEAD
-      <div className="flex justify-center space-x-2 my-4">
-      <button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-300" onClick={() => setCategoryState(0)}>All</button>
-=======
       <div className="filter space-x-2 flex justify-center ">
         <button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-300" onClick={() => setCategoryState(0)}>All</button>
->>>>>>> 03df30b4df7b5ee1c1c5a18a7c26d3bca92e18a3
 
         <button className="bg-gradient-to-r from-yellow-400 to-orange-500 hover:from-yellow-500 hover:to-orange-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300" onClick={() => setCategoryState("Alcoholic")}>Alcoholic</button>
 
