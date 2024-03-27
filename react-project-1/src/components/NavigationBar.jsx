9257ca108706f4b9dd3f391bf8b06135c43faeb5
Jasen <jasen.miyamoto@gmail.com>
2024-03-27 08:32:22 -0400

    hide game

--- react-project-1/src/components/NavigationBar.jsx
+++ react-project-1/src/components/NavigationBar.jsx
@@ -5,7 +5,9 @@ export default function NavigationBar() {
   return (
     <nav className="bg-gray-800 text-white p-4">
       <img src="https://cdn-icons-png.flaticon.com/512/5821/5821067.png" alt="Placeholder" className="absolute h-15 w-10 top-8" />
+      <NavLink to={'/memorygame'}>      
       <img src="https://cdn-icons-png.flaticon.com/512/5821/5821067.png" alt="Placeholder" className="absolute h-15 w-10 top-8 right-10" />
+      </NavLink>
       <div className="container mx-auto flex items-center justify-between">
         <NavLink to={'/'} className="text-xl font-bold">
         <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 animate-pulse shadow-lg">
@@ -31,9 +33,6 @@ export default function NavigationBar() {
 		      <NavLink to={'/myfavorites'} className="py-2 px-4 hover:bg-gray-700 rounded transition duration-300">
             My Favorites
 		      </NavLink>
-          <NavLink to={'/memorygame'} className="py-2 px-4 hover:bg-gray-700 rounded transition duration-300">
-            Drink Fortune
-          </NavLink>
         </div>
       </div>
     </nav>
